aeaa01ce2b2996f5ce0d63a296e8a5e806fa7b27
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-07-11 14:21:18 +0530

    [somu]Add:Creating Customer Table To Create ER Diagram.

---
+++ Sql_ER_DiagramProblem.sql
@@ -0,0 +1,5 @@
+Create Database Sql_ER_DiagramProblem
+
+--Creating (CUSTOMER) Table
+Create Table CUSTOMER(Customer_Id Int Identity Primary Key,
+CountryName varchar(30),MainBranch Varchar(30),Currency varchar(10));
