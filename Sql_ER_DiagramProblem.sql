b0dafb49d126cd181b347572a5ae033301c63b35
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-07-11 14:33:10 +0530

    [somu]Add:Creating Order Table and Showing Both Tables RelationShip.

--- Sql_ER_DiagramProblem.sql
+++ Sql_ER_DiagramProblem.sql
@@ -3,3 +3,7 @@ Create Database Sql_ER_DiagramProblem
 --Creating (CUSTOMER) Table
 Create Table CUSTOMER(Customer_Id Int Identity Primary Key,
 CountryName varchar(30),MainBranch Varchar(30),Currency varchar(10));
+
+--Creating (ORDERS) Table
+Create Table ORDERS(Orders_Id Int Primary Key,
+OrderName varchar(30)Not null,Customer_Id Int Foreign Key References dbo.CUSTOMER(Customer_Id));
